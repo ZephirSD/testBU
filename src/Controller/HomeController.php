d967b00d0e052ede1dfbf64789777040829b5d20
ZephirSD <87165268+ZephirSD@users.noreply.github.com>
2022-11-22 16:24:28 +0400

    Création de controller `HomeController`

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\ReservationRepository;
+
+class HomeController extends AbstractController
+{
+    #[Route('/', name: 'app_reservation_index', methods: ['GET'])]
+    public function index(ReservationRepository $reservationRepository): Response
+    {
+        return $this->render('reservation/index.html.twig', [
+            'reservations' => $reservationRepository->findAll(),
+        ]);
+    }
+}
